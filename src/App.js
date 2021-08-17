a7d61ed071ac19defbb61d5c0c89d85688d85e0c
Maciej Rapacki <mac.rapacki@gmail.com>
2021-08-17 20:27:35 +0100

    feat: create layout folder and add empty iles - footer and navbar

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import { useEffect } from "react";
 import "./styles/style.css";
 
 function App() {
-  const getSomeData = async () => {
-    const url = "https://fakestoreapi.com/products/1";
-    const response = await fetch(url);
-    if (!response.ok) {
-      const message = `status is: ${response.status}`;
-      throw new Error(message);
-    }
-    const data = await response.json();
-    console.log(data);
-    return data;
-  };
+  // const getSomeData = async () => {
+  //   const url = "https://fakestoreapi.com/products";
+  //   const response = await fetch(url);
+  //   if (!response.ok) {
+  //     const message = `status is: ${response.status}`;
+  //     throw new Error(message);
+  //   }
+  //   const data = await response.json();
+  //   console.log(data);
+  //   return data;
+  // };
 
-  useEffect(() => {
-    getSomeData();
-  }, []);
+  // useEffect(() => {
+  //   getSomeData();
+  // }, []);
   return (
     <div className="App">
       <header className="App-header"></header>
